bf53c927ba6130125f7308c9c113887c555482d2
wilsonchen <wilsonchen@superbank.com>
2019-03-29 17:22:21 +0800

    add all

---
+++ precache-manifest.d7aff661ba87ecde0ad85fb22cf5e3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactBuild20190329.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c37655eb6cb791ff959c",
+    "url": "/reactBuild20190329.io/static/js/runtime~main.ecf73997.js"
+  },
+  {
+    "revision": "764c5af668463f1727f1",
+    "url": "/reactBuild20190329.io/static/js/main.4c174142.chunk.js"
+  },
+  {
+    "revision": "24968087f19bbe0bd5a1",
+    "url": "/reactBuild20190329.io/static/js/2.710d02b6.chunk.js"
+  },
+  {
+    "revision": "764c5af668463f1727f1",
+    "url": "/reactBuild20190329.io/static/css/main.25a6ff85.chunk.css"
+  },
+  {
+    "revision": "c89f1495d48e803cb19230f2f7162ed2",
+    "url": "/reactBuild20190329.io/index.html"
+  }
+];

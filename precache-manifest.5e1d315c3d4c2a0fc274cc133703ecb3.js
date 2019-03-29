931beaa5418846da1650fc920bbca0b60edb1cfb
wilsonchen <wilsonchen@superbank.com>
2019-03-29 16:03:15 +0800

    add data

---
+++ precache-manifest.5e1d315c3d4c2a0fc274cc133703ecb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "920065aa8342e80c182b",
+    "url": "./static/js/main.83b8078b.chunk.js"
+  },
+  {
+    "revision": "24968087f19bbe0bd5a1",
+    "url": "./static/js/2.710d02b6.chunk.js"
+  },
+  {
+    "revision": "920065aa8342e80c182b",
+    "url": "./static/css/main.6cd2c938.chunk.css"
+  },
+  {
+    "revision": "6fc4feac801090bf3c1c09ad34149b00",
+    "url": "./index.html"
+  }
+];
